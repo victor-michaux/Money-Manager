149f0fe2050dbac8890cb623810c9f355ea11880
vagrant <vagrant@homestead>
2017-05-29 22:27:48 +0000

    graphique centré

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -12,12 +12,12 @@
 
 @section('content')
     <div id="app">
-        <div class="container">
+        <div class="container align-items-center">
           <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-6 mx-auto">
               <div class="card">
                 <div class="card-block">
-                  <canvas id="pieChart" width="300" height="300"></canvas>
+                  <canvas id="pieChart" style="max-width:500px; max-height:500px;"></canvas>
                 </div>
               </div>
             </div>
