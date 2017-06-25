1916a3d47e645b6acb8617f3c8eadd3c192241f2
Neonebula <bryan.boukari@gmail.com>
2017-06-25 17:18:01 -0400

    Fix stats.js

--- resources/assets/js/stats/stats.js
+++ resources/assets/js/stats/stats.js
@@ -1,29 +1,36 @@
-$.get( "ajax/test.html", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+$.getJSON( "/categories", function( donnee ) {
+  var proportionOfCategory = document.getElementById("pieChart");
+  var dataValue = [];
+  var dataLabel = [];
+  var dataColor = [];
+
+  for (var i = 0; i < donnee.length; i++) {
+    dataValue[i]=donnee[i].sumAmount;
+    dataLabel[i]=donnee[i].category.name;
+    dataColor[i]=getRandomColor();
+  }
+
+  var dataProportion = {
+    datasets: [{
+      data: dataValue,
+      backgroundColor: dataColor
+    }],
+    labels: dataLabel
+  };
+
+  var pieChart = new Chart(proportionOfCategory, {
+       type: 'doughnut',
+
+      data: dataProportion,
+
+      options: {}
+  });
 });
-
-/*
-var proportionOfCategory = document.getElementById("pieChart");
-var dataProportion = {
-  datasets: [{
-    data: [10, 20, 30]
-  }],
-  // These labels appear in the legend and in the tooltips when hovering different arcs
-  labels: [
-    'Red',
-    'Yellow',
-    'Blue'
-  ]
-};
-var pieChart = new Chart(proportionOfCategory, {
-    // The type of chart we want to create
-     type: 'doughnut',
-
-    // The data for our dataset
-    data: dataProportion,
-
-    // Configuration options go here
-    options: {}
-});
-*/
